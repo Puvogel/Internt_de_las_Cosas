41b69435cbb085fd22b4fc36165a070740ce30e9
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-11-30 17:26:33 +0000

    Update Task 2

--- Ejercicio_2/master.ino
+++ Ejercicio_2/master.ino
@@ -15,6 +15,10 @@ char serialReceivedChars[NUM_CHARS];
 
 uint16_t int_period_result;
 
+bool periodic_read_on = false;
+
+int int_delay_result = 70;
+
 
 void masterSetup(){
 
@@ -37,21 +41,21 @@ void checkSerial() {
   char endMarker = '\n';
   char rc;
   
-  while (Serial.available() > 0 && serialNewData == false) {
-    rc = Serial.read();
+  while (Serial.available() > 0) {
+      rc = Serial.read();
 
-    if (rc != endMarker) {
-        serialReceivedChars[ndx] = rc;
-        ndx++;
-        if (ndx >= numChars) {
-            ndx = numChars - 1;
-        }
-    }
-    else {
-        serialReceivedChars[ndx] = '\0'; // terminate the string
-        ndx = 0;
-        serialNewData = true;
-    }
+      if (rc != endMarker) {
+          serialReceivedChars[ndx] = rc;
+          ndx++;
+          if (ndx >= numChars) {
+              ndx = numChars - 1;
+          }
+      }
+      else {
+          serialReceivedChars[ndx] = '\0'; // terminate the string
+          ndx = 0;
+          serialNewData = true;
+      }
   }
 }
 
@@ -81,9 +85,12 @@ void checkData(){
       serialReceivedChars[0],
       serialReceivedChars[1]
       };
+    
+
 
     if (strcmp(substring0, "us") == 0){
-
+      
+      
       if (serialReceivedChars[2] == '\0') {
         header = CODE_US;
         payload[0] = 0;
@@ -97,7 +104,7 @@ void checkData(){
         printHeaderAndData();
         return;
       }
-
+      
       else{
         // take address and aconvert it to uint8
         char strHexToUint8[3] = {
@@ -127,19 +134,29 @@ void checkData(){
           serialReceivedChars[12],
           '\0'
           };
-        if (strcmp(substring1, "one-s\0") == 0){
-          
-          header = CODE_ONE_SHOT;
-          payload[2] = 0;
-          payload[1] = 0;
 
-          // receiving 4 bytes from slave from the one shot
-          dataSize = 4;
+        if (periodic_read_on == false){
+          if (strcmp(substring1, "one-s\0") == 0){
+            
+            
+            header = CODE_ONE_SHOT;
+            payload[2] = 0;
+            payload[1] = 0;
 
-          slaveDataFromSrf02 = true;
-          printHeaderAndData();
-          return;
+            // receiving 4 bytes from slave from the one shot
+            dataSize = 4;
+
+            slaveDataFromSrf02 = true;
+
+            
+
+            printHeaderAndData();
+            return;
+          }
         }
+
+
+
         char substring2[4] = {
           serialReceivedChars[8],
           serialReceivedChars[9],
@@ -150,6 +167,8 @@ void checkData(){
         if (strcmp(substring2, "on \0") == 0){
           
           header = CODE_ON_PERIOD_MS;
+          periodic_read_on = true;
+
 
           uint8_t i=11;
           char substring3[32];
@@ -162,7 +181,15 @@ void checkData(){
           int_period_result = strtol(substring3, &output, 10);
 
           Serial.print("Stored number: ");
-          Serial.println(int_period_result);
+          if (int_period_result < int_delay_result){
+            int_period_result = int_delay_result;
+            Serial.println(int_period_result);
+            Serial.println("Period is smaller than delay so setting to same value as delay");
+          }
+          else {
+            Serial.println(int_period_result);
+          }
+
 
           payload[2] = static_cast<uint8_t>(int_period_result & 0xFF);
           payload[1] = static_cast<uint8_t>((int_period_result >> 8) & 0xFF);
@@ -179,6 +206,8 @@ void checkData(){
         if (strcmp(substring2, "off\0") == 0){
           
           header = CODE_OFF;
+          periodic_read_on = false;
+
           payload[2] = 0;
           payload[1] = 0;
           
@@ -251,10 +280,18 @@ void checkData(){
             i++;
           }
 
-          int int_delay_result = strtol(substring3, &output, 10);
+          int_delay_result = strtol(substring3, &output, 10);
 
           Serial.print("Stored number: ");
-          Serial.println(int_delay_result);
+          if (int_delay_result < 70){ 
+            Serial.println("value to small");
+            Serial.println("Defaulting to 70");
+            int_delay_result = 70;
+          }
+          else {
+            Serial.println(int_delay_result);
+          }
+          
 
           size_t arraySize = sizeof(int);
 
@@ -269,22 +306,22 @@ void checkData(){
           return;
         }      
 
+        if (periodic_read_on == false){
+          if (strcmp(substring5, "status\0") == 0){
+            
+            header = CODE_STATUS;
+            payload[2] = 0;
+            payload[1] = 0;
 
-        if (strcmp(substring5, "status\0") == 0){
-          
-          header = CODE_STATUS;
-          payload[2] = 0;
-          payload[1] = 0;
+            // receiving 7 bytes from slave from the status
+            dataSize = 7;
 
-          // receiving 7 bytes from slave from the status
-          dataSize = 7;
+            slaveDataFromSrf02 = true;
 
-          slaveDataFromSrf02 = true;
-
-          printHeaderAndData();
-          return;
-        }                     
-
+            printHeaderAndData();
+            return;
+          }                     
+        }
       }
     }
     Serial.println("Use 'help' to view available commands");
@@ -297,6 +334,8 @@ void sendingData() {
   if (serialNewData == true) {
     Serial.println("sending..."); 
 
+    //DESCOMENTAR
+    //////////////////////////////////////////////////////////////////
     // call second function to write option code on Serial1
     
     serial1WriteHeader();
@@ -304,8 +343,11 @@ void sendingData() {
     
     // call second function to write data on Serial1
     serial1WriteData();
+    //////////////////////////////////////////////////////////////////
+    
 
     serialReceivedChars[numChars] = {};
+    delay(1500);
   }
 }
 
@@ -333,7 +375,26 @@ void serial1WriteData(){
   Serial1.write(payload[1]);
   Serial1.write(payload[2]);
 
-  Serial1.flush();  
+  Serial1.flush();
+
+
+  
+/*
+  acknowledge = 0;
+
+  // Waiting for the Option acknowledge
+  if(Serial1.available()>0)
+  {
+    uint8_t acknowledge=Serial1.read();
+    if (acknowledge == CODE_ACK)
+    {
+      Serial.print("right acknowledge: "); Serial.println(static_cast<int>(acknowledge));
+    }
+    else {
+      Serial.print("wrong acknowledge: "); Serial.println(static_cast<int>(acknowledge));
+    }
+  }*/
+  
 }
 
 void printHeaderAndData(){
@@ -351,13 +412,13 @@ void intToUint8Array(int value, uint8_t* array, size_t size) {
 
 void receivingData(){
   
-  if (serialNewData == true){
-    if (header == CODE_ONE_SHOT){
-      byte dataFromSlave[dataSize] = {};
-        
-      if(Serial1.available()>0){
-        Serial1.readBytes(dataFromSlave, dataSize);
-      }
+  if (periodic_read_on == true){
+    
+    byte dataFromSlave[dataSize] = {};
+
+    if(Serial1.available()>0){
+      Serial1.readBytes(dataFromSlave, dataSize);
+    
 
       uint16_t distance = (dataFromSlave[1] << 8) | dataFromSlave[0];
       uint16_t range = (dataFromSlave[3] << 8) | dataFromSlave[2];
@@ -366,66 +427,107 @@ void receivingData(){
       Serial.println(distance);
       Serial.println("rangeMin: ");
       Serial.println(range);
-
     }
-  
-    if (header == CODE_US){
-      byte dataFromSlave[dataSize] = {};
-
-      if(Serial1.available()>0){
-        Serial1.readBytes(dataFromSlave, dataSize);
-      }
-
-      uint16_t range = (dataFromSlave[1] << 8) | dataFromSlave[0];
-
-      Serial.println("addresses: ");
-      Serial.println(dataFromSlave[0]);
-      Serial.println(dataFromSlave[1]);
-    }
-
-    if (header == CODE_ON_PERIOD_MS){
-      byte dataFromSlave[dataSize] = {};
-      
-      last_ms = millis();
-      new_ms = millis();
-      while (new_ms-last_ms < int_period_result){
+    
+    
+  } else {
+    if (serialNewData == true){
+      if (header == CODE_ONE_SHOT){
+        byte dataFromSlave[dataSize] = {};
+          
         if(Serial1.available()>0){
           Serial1.readBytes(dataFromSlave, dataSize);
         }
 
-        if (Serial.available()){
-          serialNewData = false;
-          return;
-        }
-
-        uint16_t distance = (dataFromSlave[1] << 8) | dataFromSlave[0];
-        uint16_t range = (dataFromSlave[3] << 8) | dataFromSlave[2];
+        uint16_t range = (dataFromSlave[1] << 8) | dataFromSlave[0];
+        uint16_t distance = (dataFromSlave[3] << 8) | dataFromSlave[2];
 
         Serial.println("distancia: ");
         Serial.println(distance);
         Serial.println("rangeMin: ");
         Serial.println(range);
-        new_ms = millis();
-      }
-    }
 
-    if (header == CODE_STATUS){
-      byte dataFromSlave[dataSize] = {};
+      }
+    
+      if (header == CODE_US){
+        byte dataFromSlave[dataSize] = {};
+        byte dataFromSlave2[dataSize] = {};
+
+        int i = 0;
+        while(Serial1.available()>0){
+          dataFromSlave2[i] = Serial1.read();
+          i++;
+          i= i%2;
+        }
+        
+        uint8_t exchange=0;
+        for(int j=0;j<dataSize;j++){
+          dataFromSlave[j] = dataFromSlave2[(j + i)%2];
+        }
+
+
+        uint16_t range = (dataFromSlave[1] << 8) | dataFromSlave[0];
+
+        Serial.println("addresses: ");
+        Serial.println(dataFromSlave[0], HEX);
+        Serial.println(dataFromSlave[1], HEX);
+      }
+
       
-      if(Serial1.available()>0){
-        Serial1.readBytes(dataFromSlave, dataSize);
+
+      if (header == CODE_STATUS){
+        byte dataFromSlave[dataSize] = {};
+        byte dataFromSlave2[dataSize] = {};
+
+        int i = 0;
+        while(Serial1.available()>0){
+          dataFromSlave2[i] = Serial1.read();
+          i++;
+          i= i%7;
+        }
+        
+        uint8_t exchange=0;
+        for(int j=0;j<dataSize;j++){
+          dataFromSlave[j] = dataFromSlave2[(j + i)%7];
+        }
+        
+        
+
+        uint16_t sensorAdress = (dataFromSlave[1] << 8) | dataFromSlave[0];
+        uint16_t sensorDelay = (dataFromSlave[3] << 8) | dataFromSlave[2];
+        uint16_t sensorPeriod = (dataFromSlave[5] << 8) | dataFromSlave[4];
+
+        Serial.println("Status: ");
+        
+        Serial.print("Sensor address: ");
+        Serial.println(sensorAdress, HEX);
+        Serial.print("Sensor delay: ");
+        Serial.println(sensorDelay);
+        Serial.print("Sensor period: ");
+        Serial.println(sensorPeriod);
+        Serial.print("Sensor unit: ");
+
+        if (dataFromSlave[6] == 80) {Serial.println("Unit inches");}
+        if (dataFromSlave[6] == 81) {Serial.println("Unit cms");}
+        if (dataFromSlave[6] == 82) {Serial.println("Unit ms");}
+        
       }
 
-      uint16_t range = (dataFromSlave[1] << 8) | dataFromSlave[0];
-
-      Serial.println("Status: ");
-      Serial.println(dataFromSlave[0]);
-      Serial.println(dataFromSlave[1]);
-      Serial.println(dataFromSlave[2]);
-      Serial.println(dataFromSlave[3]);
-      Serial.println(dataFromSlave[4]);
-      Serial.println(dataFromSlave[5]);
-      Serial.println(dataFromSlave[6]);
+      /*
+      if (header == CODE_ON_PERIOD_MS){
+        last_ms=millis();
+        new_ms=millis();
+        byte dataFromSlave[dataSize] = {};
+        while (new_ms-last_ms < int_period_result){
+          if(Serial1.available()>0)
+          {
+            Serial1.readBytes(dataFromSlave, dataSize);
+            Serial.println(dataFromSlave);
+          }
+          new_ms=millis();
+        }
+      }
+      */
     }
   }
   serialNewData = false;
--- Ejercicio_2/slave.ino
+++ Ejercicio_2/slave.ino
@@ -1,65 +1,91 @@
+/////////////////////
+/// tc_lib config ///
+/////////////////////
+#define ARDUINO_MKR_PORT
+
+#include "tc_lib.h"
+
+using namespace tc_lib::arduino_mkr;
+
+#define CALLBACK_PERIOD 50000000 // hundreths of usecs. (1e-8 secs.)
+#define DELAY_TIME 1000 // msecs.
+
+// action_tc4 declaration
+action_tc4_declaration();
+
+struct ctx
+{
+  ctx() { doMeasurement=false; counter=0; }
+
+  bool doMeasurement;
+  volatile uint32_t counter;
+};
+
+ctx action_ctx;
+
+
 ////////////////////
-  /// OLED Display ///
-  ////////////////////
+/// OLED Display ///
+////////////////////
 
-  #define SCREEN_WIDTH 128 // OLED display width, in pixels
-  #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-  time_t lastTimeOLED_ms = 0;           // Variable to hold a time stamp in milli seconds to time OLED refreshing
-  #define OLED_REFRESH_RATE_MS 100     // Define refreshrate of OLED in ms
+time_t lastTimeOLED_ms = 0;           // Variable to hold a time stamp in milli seconds to time OLED refreshing
+#define OLED_REFRESH_RATE_MS 100     // Define refreshrate of OLED in ms
 
-  // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-  // The pins for I2C are defined by the Wire-library. 
-  // On an arduino UNO:       A4(SDA), A5(SCL)
-  // On an arduino MEGA 2560: 20(SDA), 21(SCL)
-  // On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-  #define OLED_RESET     -1     // Reset pin # (or -1 if sharing Arduino reset pin)
-  #define SCREEN_ADDRESS 0x3D   // /< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-  Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1     // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D   // /< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 
-  //////////////////////
-  /// Sensor reading ///
-  //////////////////////
+//////////////////////
+/// Sensor reading ///
+//////////////////////
 
-  #define SENSOR_ACTIVE true
+#define SENSOR_ACTIVE true
 
-  time_t lastTimeSendor_ms = 0;      // Variable to hold a time stamp in milli seconds to time sensor refreshing
+time_t lastTimeSendor_ms = 0;      // Variable to hold a time stamp in milli seconds to time sensor refreshing
 
-  #define SRF02_I2C_ADDRESS byte((0xE0)>>1)
-  #define SRF02_I2C_INIT_DELAY 100    // in milliseconds
-  #define SRF02_RANGING_DELAY 70      // milliseconds
-  #define SRF02_MEASURE_DELAY 500     // milliseconds
+#define SRF02_I2C_ADDRESS byte((0xE0)>>1)
+#define SRF02_I2C_INIT_DELAY 100    // in milliseconds
+#define SRF02_RANGING_DELAY 70      // milliseconds
+#define SRF02_MEASURE_DELAY 500     // milliseconds
 
-  // LCD05's command related definitions        // TODO: Remove if not necessary
-  #define COMMAND_REGISTER byte(0x00)
-  #define SOFTWARE_REVISION byte(0x00)
-  #define RANGE_HIGH_BYTE byte(2)
-  #define RANGE_LOW_BYTE byte(3)
-  #define AUTOTUNE_MINIMUM_HIGH_BYTE byte(4)
-  #define AUTOTUNE_MINIMUM_LOW_BYTE byte(5)
+// LCD05's command related definitions        // TODO: Remove if not necessary
+#define COMMAND_REGISTER byte(0x00)
+#define SOFTWARE_REVISION byte(0x00)
+#define RANGE_HIGH_BYTE byte(2)
+#define RANGE_LOW_BYTE byte(3)
+#define AUTOTUNE_MINIMUM_HIGH_BYTE byte(4)
+#define AUTOTUNE_MINIMUM_LOW_BYTE byte(5)
 
-  // SRF02's command codes
-  #define REAL_RANGING_MODE_INCHES    byte(80)
-  #define REAL_RANGING_MODE_CMS       byte(81)
-  #define REAL_RANGING_MODE_USECS     byte(82)
-  #define FAKE_RANGING_MODE_INCHES    byte(86)
-  #define FAKE_RANGING_MODE_CMS       byte(87)
-  #define FAKE_RANGING_MODE_USECS     byte(88)
-  #define TRANSMIT_8CYCLE_40KHZ_BURST byte(92)
-  #define FORCE_AUTOTUNE_RESTART      byte(96)
-  #define ADDRESS_CHANGE_1ST_SEQUENCE byte(160)
-  #define ADDRESS_CHANGE_3RD_SEQUENCE byte(165)
-  #define ADDRESS_CHANGE_2ND_SEQUENCE byte(170)
+// SRF02's command codes
+#define REAL_RANGING_MODE_INCHES    byte(80)
+#define REAL_RANGING_MODE_CMS       byte(81)
+#define REAL_RANGING_MODE_USECS     byte(82)
+#define FAKE_RANGING_MODE_INCHES    byte(86)
+#define FAKE_RANGING_MODE_CMS       byte(87)
+#define FAKE_RANGING_MODE_USECS     byte(88)
+#define TRANSMIT_8CYCLE_40KHZ_BURST byte(92)
+#define FORCE_AUTOTUNE_RESTART      byte(96)
+#define ADDRESS_CHANGE_1ST_SEQUENCE byte(160)
+#define ADDRESS_CHANGE_3RD_SEQUENCE byte(165)
+#define ADDRESS_CHANGE_2ND_SEQUENCE byte(170)
 
-  uint16_t srf_range = 0;     // Global variable to hold range measurement of ultrasonic sensor
-  uint16_t srf_min = 0;       // Global variable to hold min-range measurement of ultrasonic sensor
+uint16_t srf_range = 0;     // Global variable to hold range measurement of ultrasonic sensor
+uint16_t srf_min = 0;       // Global variable to hold min-range measurement of ultrasonic sensor
 
-  // Flag to show that periodic sensor reading is active
-  bool _is_periodic_ranging = false;
-  time_t sensorFirstTime = 0;
-  time_t sensorLastTime = millis();
+// Flag to show that periodic sensor reading is active
+bool _is_periodic_ranging = false;
+time_t sensorFirstTime = 0;
+time_t sensorLastTime = millis();
 
   inline void write_command(byte address,byte command)
   { 
@@ -105,18 +131,13 @@
     USEC
   };
 
-  uint8_t sensorAddress = SRF02_I2C_ADDRESS;
-  uint16_t sensorPeriod_ms = 10000;                     // Default value for debugging
-  uint16_t sensorDelay_ms = SRF02_MEASURE_DELAY;
+  uint16_t sensorAddress = SRF02_I2C_ADDRESS;
+  uint16_t sensorPeriod_ms = 500;                      // Default value for debugging
+  uint16_t effectiveSensorPeriod_ms = sensorPeriod_ms;
+  uint16_t sensorDelay_ms = SRF02_RANGING_DELAY;
   uint8_t sensorUnit = REAL_RANGING_MODE_CMS;
 
 void slaveSetup(){
-  // DEBUGGING
-  //srf_range = 357;    // Mockup values since we cannto use sensor
-  //srf_min = 73;       // Mockup values since we cannto use sensor
-  //_is_periodic_ranging = true;
-  //SerialUSB.println("DEBUGGING: Strating period sensor reading once...");
-
   Serial.println("waiting...");
 
   ////////////////////
@@ -156,6 +177,7 @@ void slaveSetup(){
 
     Serial.println("Finished Sensor setup ...");
   }
+  Serial.println("Waiting for command form master...");
 }
 
 
@@ -185,16 +207,9 @@ void slaveLoop(){
 
   // If a period for sensor readout is demanded, enter here
   if(_is_periodic_ranging) {
-    time_t cur_time = millis();
-    // Read sensor if delay has passed
-    if (cur_time - sensorLastTime > sensorDelay_ms) {
-      sensorLastTime = millis();    // Set last time variable to curent time
-      com_srfOneShot();             // Execute sensor reading (will send data to master)
-    }
-    // Determine if sensor ranging time has passed
-    if (cur_time - sensorFirstTime > sensorPeriod_ms) {
-      _is_periodic_ranging = false; // stop periodic sensor reading
-    }
+    com_srfOneShot();             // Execute sensor reading (will send data to master)
+    //SerialUSB.println("DEBUGGING: Resetting Flag to false...");
+    _is_periodic_ranging = false; // Reset flag
   }
 }
 
@@ -208,9 +223,6 @@ void slaveLoop(){
 
 void checkSerial1(){
 
-  
-
-  
   if(Serial1.available()>0){
       
     header = Serial1.read();
@@ -265,11 +277,17 @@ void checkSerial1(){
       uint16_t period = (data[1] << 8) | data[2];
       Serial.print("Period for sensor: ");
       Serial.println(period);
-      com_srfOnPeriod_ms(period);    // Commented out because of lack of cables to connect sensor
+      //Setting specified period
+      com_setPeriod(period);
+      uint_fast32_t newPeriod = sensorPeriod_ms * 100000;           // converting to required 1e-8 secs format
+      action_tc4.stop();  // Stopping possible timer
+      action_tc4.start(newPeriod, set_range_action,&action_ctx);
     }else if (header == CODE_OFF){
       //Turn off periodic sensor measuring
       Serial.println("Stopping sensor reading...");
+      action_tc4.stop();
       _is_periodic_ranging = false;
+      Serial1.write(CODE_ACK);          // Sending ACK
     }else if (header == CODE_STATUS){
       Serial.println("Sending Status to master...");
       com_retrieveStatus();
@@ -281,8 +299,6 @@ void checkSerial1(){
       // Change delay/interval for sensor reading
       Serial.println("Received request to change delay for sensor. Changing...");
       uint16_t delay = (data[1] << 8) | data[2];
-      Serial.print("Delay for sensor: ");
-      Serial.println(delay);
       com_setDelay(data[0], delay);
     }else if (header == CODE_US){
       // Send all available sensor addresses to master
@@ -367,15 +383,7 @@ void checkSerial1(){
     // Or display request status
     if (_is_periodic_ranging) {      
       display.setCursor(10, display.getCursorY());
-      display.println("Measuring for: ");
-      display.setCursor(10, display.getCursorY());
-      display.print(sensorPeriod_ms);
-      display.println("ms");
-      display.setCursor(10, display.getCursorY());
-      display.println("Remaining time: ");
-      display.setCursor(10, display.getCursorY());
-      display.print(-(millis() - sensorPeriod_ms - sensorFirstTime));
-      display.println("ms");
+      display.println("Measuring continously...");
     }else {
       display.setCursor(10, display.getCursorY());
       display.println("No request! ");
@@ -394,41 +402,57 @@ void checkSerial1(){
   // Send data to master
   void com_srfOneShot(){
     if (SENSOR_ACTIVE) {
+      // Do a new measurment
+      Serial.println("Taking measurment...");
+      write_command(sensorAddress, sensorUnit);
+
+      delay(sensorDelay_ms);
+
       Serial.println("Reading sensor...");
       digitalWrite(LED_BUILTIN, HIGH);
-      write_command(sensorAddress,sensorUnit);
-        // Delay to make accurate reading -> see manual
-        delay(SRF02_RANGING_DELAY);
+      SerialUSB.print("SensorUnit: ");
+      SerialUSB.println(sensorUnit);
 
-        byte high_byte_range=read_register(SRF02_I2C_ADDRESS,RANGE_HIGH_BYTE);
-        byte low_byte_range=read_register(SRF02_I2C_ADDRESS,RANGE_LOW_BYTE);
-        byte high_min=read_register(SRF02_I2C_ADDRESS,AUTOTUNE_MINIMUM_HIGH_BYTE);
-        byte low_min=read_register(SRF02_I2C_ADDRESS,AUTOTUNE_MINIMUM_LOW_BYTE);
+      byte high_byte_range=read_register(SRF02_I2C_ADDRESS,RANGE_HIGH_BYTE);
+      byte low_byte_range=read_register(SRF02_I2C_ADDRESS,RANGE_LOW_BYTE);
+      byte high_min=read_register(SRF02_I2C_ADDRESS,AUTOTUNE_MINIMUM_HIGH_BYTE);
+      byte low_min=read_register(SRF02_I2C_ADDRESS,AUTOTUNE_MINIMUM_LOW_BYTE);
 
-        srf_range = int16_t((high_byte_range<<8) | low_byte_range);
-        srf_min = int16_t((high_min<<8) | low_min);
-        digitalWrite(LED_BUILTIN, LOW);
+      srf_range = int16_t((high_byte_range<<8) | low_byte_range);
+      srf_min = int16_t((high_min<<8) | low_min);
+      digitalWrite(LED_BUILTIN, LOW);
 
-        uint8_t rspData[4];
-        rspData[0] = srf_range & 0xFF;          // Lower 8 bits of srf_range
-        rspData[1] = (srf_range >> 8) & 0xFF;   // Upper 8 bits of srf_range
+      uint8_t rspData[4];
+      rspData[0] = srf_range & 0xFF;          // Lower 8 bits of srf_range
+      rspData[1] = (srf_range >> 8) & 0xFF;   // Upper 8 bits of srf_range
 
-        rspData[2] = srf_min & 0xFF;            // Lower 8 bits of srf_min
-        rspData[3] = (srf_min >> 8) & 0xFF;     // Upper 8 bits of srf_min
-        sendDataToMaster(rspData, sizeof(rspData));
+      rspData[2] = srf_min & 0xFF;            // Lower 8 bits of srf_min
+      rspData[3] = (srf_min >> 8) & 0xFF;     // Upper 8 bits of srf_min
+      sendDataToMaster(rspData, sizeof(rspData));
     }else {
       Serial.println("Sensor not working, sending mockup data...");
-      uint8_t rspData[] = {0, 255, 0, 22};          // because of lack of cables to connect sensor
+      delay(sensorDelay_ms);
+      uint8_t rspData[4];
+      rspData[0] = srf_range & 0xFF;          // Lower 8 bits of srf_range
+      rspData[1] = (srf_range >> 8) & 0xFF;   // Upper 8 bits of srf_range
+
+      rspData[2] = srf_min & 0xFF;            // Lower 8 bits of srf_min
+      rspData[3] = (srf_min >> 8) & 0xFF;     // Upper 8 bits of srf_min          // because of lack of cables to connect sensor
       sendDataToMaster(rspData, sizeof(rspData));   // because of lack of cables to connect sensor
     }
   }
 
-  void com_srfOnPeriod_ms(uint16_t period_ms) {
-    _is_periodic_ranging = true;
-    sensorPeriod_ms = period_ms;
-    sensorFirstTime = millis();         // Set starting time for periodic sensor reading
-    sensorLastTime = sensorFirstTime;   // Reset variable
-  }
+// Change the flag to do a periodic reading
+void set_range_action(void* a_ctx)
+{
+  //ctx* the_ctx=reinterpret_cast<ctx*>(a_ctx);
+  //
+  //digitalWrite(LED_BUILTIN,((the_ctx->onoff)? HIGH: LOW));
+  //the_ctx->onoff=!(the_ctx->onoff);
+  //
+  //the_ctx->counter++;
+  _is_periodic_ranging = true;
+}
 
   // Take all available inforamtion suh as sensor address, configured delay and period as well as selected unit format and send information to master
   void com_retrieveStatus(){
@@ -440,25 +464,45 @@ void checkSerial1(){
     rspData[2] = sensorDelay_ms & 0xFF;         // Lower 8 bits of sensorDelay_ms
     rspData[3] = (sensorDelay_ms >> 8) & 0xFF;  // Upper 8 bits of sensorDelay_ms
 
-    rspData[4] = sensorPeriod_ms & 0xFF;        // Lower 8 bits of sensorPeriod_ms
-    rspData[5] = (sensorPeriod_ms >> 8) & 0xFF; // Upper 8 bits of sensorPeriod_ms
+    rspData[4] = effectiveSensorPeriod_ms & 0xFF;        // Lower 8 bits of sensorPeriod_ms
+    rspData[5] = (effectiveSensorPeriod_ms >> 8) & 0xFF; // Upper 8 bits of sensorPeriod_ms
 
     rspData[6] = sensorUnit;                    // Already uint8_t
 
     sendDataToMaster(rspData, arr_size);
   }
 
-  // Set delay between distinct sensor readings
-  void com_setDelay(uint8_t address, uint16_t delay){
+  // Set delay between measuring and reading sensor
+  void com_setDelay(uint16_t address, uint16_t delay){
+    if (delay < 70) {
+      Serial.println("Specified delay too small, defaulting to 70ms");
+      sensorDelay_ms = 70;
+    }else {
+      sensorDelay_ms = delay;
+      Serial.print("Setting delay to: ");
+      Serial.println(sensorDelay_ms);
+    }
     sensorAddress = address;
-    sensorDelay_ms = delay;
-    Serial.print("Setting delay to: ");
-    Serial.println(sensorDelay_ms);
+  }
+
+  // Set period between distinct sensor readings
+  // Will subtract the sensorDelay_ms time to get accurate period time
+  void com_setPeriod(uint16_t period){
+    if (period <= sensorDelay_ms) {
+      Serial.println("Specified period too small, defaulting to value of set delay");
+      sensorPeriod_ms = 1;
+      effectiveSensorPeriod_ms = sensorDelay_ms;                      // For easier printouting real period
+    }else {
+      sensorPeriod_ms = period + 1 - sensorDelay_ms;
+      effectiveSensorPeriod_ms = sensorPeriod_ms + sensorDelay_ms;    // For easier printouting real period
+      Serial.print("Setting effective period to: ");
+      Serial.println(sensorPeriod_ms + sensorDelay_ms);
+    }
   }
 
   // Set unit for sensor reading
   // Will halt if unit can not be determined;
-  void com_setUnit(uint8_t address, uint8_t unit){
+  void com_setUnit(uint16_t address, uint8_t unit){
     Serial.print("Unit received: ");
     Serial.println(unit);
     sensorAddress = address;
